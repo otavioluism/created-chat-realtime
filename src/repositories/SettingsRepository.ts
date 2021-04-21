373a1831df5c777e4fab7a6c0b72e40f983f0319
Otávio Luis Martins <otavioluism97@hotmail.com>
2021-04-21 18:34:51 -0300

    Criado tabela de Settings com sua migration

---
+++ src/repositories/SettingsRepository.ts
@@ -0,0 +1,7 @@
+import { Repository, EntityRepository } from 'typeorm';
+import Setting from '../entities/Setting';
+
+@EntityRepository(Setting)
+export default class SettingsRepository extends Repository<Setting> { 
+
+}
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import express from 'express'; 
+import "./database";
+
+import routes from './routes';
 
 const app = express(); 
 
-app.get("/", (request, response) => { 
-  return response.status(201).json({ message: "Hello World"})
-})
+app.use(express.json()); // habilitar o recebimento das requisições pelo body
 
+app.use(routes)
 
 app.listen(3333, () => { 
   console.log('Server running in port 3333!');
-})
+});
